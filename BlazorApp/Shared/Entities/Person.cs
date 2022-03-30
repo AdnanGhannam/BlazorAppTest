8b5ce52db67674994d6cd84100c69160ebe9d018
Adnan <adnanghannam21@gmail.com>
2022-03-30 23:33:25 +0300

    Entities added

---
+++ BlazorApp/Shared/Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared.Entities
+{
+    public class Person
+    {
+        const string DefaultName = "DefaultPersonName";
+
+        public int Id { get; private set; }
+
+        public string Name { get; private set; } = DefaultName;
+    }
+}
